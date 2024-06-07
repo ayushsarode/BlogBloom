c8ede826600164b4f62de9552d87215c5f3c2e33
Ayush Sarode <72145248+AyushSarode@users.noreply.github.com>
2024-06-08 00:09:56 +0530

    signup

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express')
 const path = require('path')
+const userRoute = require('./routes/user')
+const mongoose = require('mongoose')
 
 const app = express()
 PORT = 8001;
 
+mongoose.connect('mongodb://localhost:27017/bloomblog')
+    .then(() => {
+        console.log('MongoDB server is running');
+    })
+    .catch(err => {
+        console.error('Connection error', err);
+    });
+
+
+
 app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({ extended: false }));
+app.use('/user', userRoute)
 
-app.get('/', (req,res)=> {
+app.get('/', (req, res) => {
     res.render('home')
 })
 
 
-app.listen(PORT, ()=> console.log(`Server is running at http://localhost:8001`))
+app.listen(PORT, () => console.log(`Server is running at http://localhost:8001`))
