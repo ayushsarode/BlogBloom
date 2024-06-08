dd310899fe26d0dd44358a096012c7fda7b6d872
Ayush Sarode <72145248+AyushSarode@users.noreply.github.com>
2024-06-08 15:31:17 +0530

    auth

--- routes/user.js
+++ routes/user.js
@@ -4,22 +4,30 @@ const User = require('../models/user')
 const router = Router()
 
 router.get("/signin", (req, res) => {
-    return res.render("signin");
-  });
+  return res.render("signin");
+});
+
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.matchPassword(email, password)
+
+  console.log("User", user);
+  return res.redirect('/')
+})
 
 router.get("/signup", (req, res) => {
-    return res.render("signup");
-  });
+  return res.render("signup");
+});
 
 router.post("/signup", async (req, res) => {
-    const { fullName, email, password } = req.body;
-    await User.create({
-      fullName,
-      email,
-      password,
-    });
-    return res.redirect("/");
+  const { fullName, email, password } = req.body;
+  await User.create({
+    fullName,
+    email,
+    password,
   });
+  return res.redirect("/");
+});
 
 
 module.exports = router
