5b9455c651f7977c59af712f12c7df2c30f5f486
Ayush Sarode <72145248+AyushSarode@users.noreply.github.com>
2024-06-11 00:14:26 +0530

    auth

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -2,22 +2,18 @@ const { validateToken } = require('../service/auth');
 
 function checkForAuthenticationCookie(cookieName) {
     return (req, res, next) => {
-        const tokenCookieValue = req.cookies[cookieName]; 
-
-        if (!tokenCookieValue) {
-            return res.status(401).json({ error: 'No token provided' });
-        }
-
-        try {
-            const userPayload = validateToken(tokenCookieValue);
-            req.user = userPayload;
-            next();
-        } catch (error) {
-            console.error('Token validation error:', error);
-            return res.status(403).json({ error: 'Invalid token' });
-        }
+      const tokenCookieValue = req.cookies[cookieName];
+      if (!tokenCookieValue) {
+        return next();
+      }
+      try {
+        const userPayload = validateToken(tokenCookieValue);
+        req.user = userPayload;
+      } catch (error) {}
+  
+      return next();
     };
-}
+  }
 
 module.exports = {
     checkForAuthenticationCookie,
--- service/auth.js
+++ service/auth.js
@@ -11,20 +11,17 @@ function createTokenForUser(user) {
         profileImageURL: user.profileImageURL,
         role: user.role,
     };
-    
-    // Optionally, add an expiration time (e.g., 1 hour)
     const token = JWT.sign(payload, secret, { expiresIn: '1h' });
     
     return token;
 }
 
-// Validate a token and return the payload
+
 function validateToken(token) {
     try {
         const payload = JWT.verify(token, secret);
         return payload;
     } catch (error) {
-        // Handle token verification errors
         console.error('Token validation error:', error);
         throw new Error('Invalid token');
     }
